ad72d92251cd1f818a85a196e0778bd0f09df9d2
Steve Warren <steve@speakaboos.com>
2013-09-27 17:46:33 -0400

    trying to stabilize the build process

--- ANESampleBuild/buildANE.sh
+++ ANESampleBuild/buildANE.sh
@@ -1,5 +1,5 @@
-adt="/Applications/Adobe Flash Builder 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/bin/adt"
-nativedir="/Users/stevewarren/Clients/Speakaboos/git_repositories/ANESample_java"
+adt='/Applications/Adobe Flash Builder 4.7/eclipse/plugins/com.adobe.flash.compiler_4.7.0.349722/AIRSDK/bin/adt'
+nativedir=/Users/stevewarren/Clients/Speakaboos/git_repositories/ANESampleProject/ANESample_java
 
 echo "********************************************************************"
 echo " - creating ANE package"
@@ -10,10 +10,14 @@ mkdir -p Android-ARM
 
 unzip ../ANESample/bin/ANESample.swc library.swf
 cp library.swf Android-ARM
-cp "$nativedir"/ANESample.jar Android-ARM
-cp -r "$nativedir"/res Android-ARM
+cp library.swf ../ANESample/bin
+cp $nativedir/ANESample.jar Android-ARM
+cp -r $nativedir/res Android-ARM
 
-"$adt" -package -target ane SampleASExtension.ane extension.xml -swc ../ANESample/bin/ANESample.swc -platform Android-ARM -C Android-ARM .
+#Example from http://esdot.ca/site/2012/tutorial-packaging-anes
+#"%ADT%" -package -target ane "%OUTPUT%" "%XML%" -swc "%SWC%" %ANDROID_PLATFORM% %DEFAULT_PLATFORM%
+
+"$adt" -package -target ane com.adobe.sampleasextension.SampleASExtension.ane extension.xml -swc ../ANESample/bin/ANESample.swc -platform Android-ARM -C Android-ARM . -platform default -C ../ANESample/bin .
 
 #TO PACKAGE WITH CODE SIGNING
 #"$adt" -package -storetype PKCS12 -keystore cer.p12 -storepass password -target ane SampleASExtension.ane extension.xml -swc ../ANESample/bin/ANESample.swc -platform Android-ARM -C Android-ARM .
