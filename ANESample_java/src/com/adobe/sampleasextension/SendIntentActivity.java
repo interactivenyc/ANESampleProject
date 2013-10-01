dfd7cc2b1ee9bd17ab8c7bca7ac7fb10de93a081
Steve Warren <steve@speakaboos.com>
2013-09-30 21:58:30 -0400

    work in progress. app not functioning yet. looking for advice

---
+++ ANESample_java/src/com/adobe/sampleasextension/SendIntentActivity.java
@@ -0,0 +1,70 @@
+package com.adobe.sampleasextension;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SendIntentActivity extends Activity {
+
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        log("SendIntentActivity.onCreate");
+	        
+	        Intent intent = getIntent();
+	        String action = intent.getAction();
+	        String type = intent.getType();
+	        
+	        
+
+	        //if (Intent.ACTION_SEND.equals(action) && type != null) {            
+	            this.handleSend(intent);
+	        //}
+		}
+	 
+		 protected void handleSend(Intent intent){
+			 log("SendIntentActivity.onCreate");
+		 }
+
+
+	    @Override
+	    protected void onStart() {
+	    	super.onStart();
+	    	log( "SendIntentActivity.onStart" );
+	    }
+	    
+	    @Override
+	    protected void onRestart() {
+	    	super.onRestart();
+	    	log( "SendIntentActivity.onRestart" );
+	    }
+
+	    @Override
+	    protected void onResume() {
+	    	super.onResume();
+	    	log( "SendIntentActivity.onResume" );
+	    }
+
+	    @Override
+	    protected void onPause() {
+	    	super.onPause();
+	    	log( "brosenqu SendIntentActivity.onPause" );
+	    }
+
+	    @Override
+	    protected void onStop() {
+	    	super.onStop();
+	    	log( "SendIntentActivity.onStop" );
+	    }
+
+	    @Override
+	    protected void onDestroy() {
+	    	super.onDestroy();
+	    	log( "SendIntentActivity.onDestroy" );
+	    }	
+
+	 	public void log(String msg){
+			System.out.println("[ SendIntentActivity ] "+msg);
+		}
+
+}
